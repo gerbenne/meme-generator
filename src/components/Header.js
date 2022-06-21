0dee8a46904671b2253bd5c4e858290ea71db8f9
agerbenne <anthony.gerbenne@gmail.com>
2022-06-21 17:59:51 +0200

    Create meme generator

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img
+                src="./images/troll.png"
+                className="header--image"
+                alt="troll"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+        </header>
+    )
+}
